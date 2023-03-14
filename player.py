832e31497dcc0b1ea8db791fa852557f08917500
David Sturgis <david-sturgis@pluralsight.com>
2023-03-14 01:18:20 +0000

    WIP from the March 13th session
    Won in an average of 392 guesses per game!
       Minimum Guesses: 1
       Maximum Guesses: 1,797
           Total Games: 500
         Total Guesses: 196,141

--- player.py
+++ player.py
@@ -1,21 +1,53 @@
 import random
+import string
 
 
 class Player:
 
     def __init__(self):
-        # Implement internal state here
-        self.state = None
+        # Implement internal state here?
+        self.last_returned_guess = 0
+        self.available_digits = set(string.digits)
 
     def guess(self):
+        """
+        Guess four digits numbers, expressed as strings (e.g. "0146")
+        (also: track any necessary state?)
+        """
         # Generate a random number between 0 and 9,999
-        integer_guess = int(random.random() * 10000)
+        # integer_guess = int(random.random() * 10000)
         # Convert to string
-        guess = str(integer_guess)
+        # guess = str(integer_guess)
         # Right-justify with zeroes, e.g. "146" -> "0146"
-        padded_guess = guess.rjust(4, "0")
-        return padded_guess
+        # padded_guess = guess.rjust(4, "0")
+        # This is not a very good guess?
+        # return padded_guess
+        for n in range(self.last_returned_guess + 1, 10000):
+            padded_guess = str(n).rjust(4, "0")
+            padded_guess_chars = set(padded_guess)
+
+            valid_chars = len(padded_guess_chars.intersection(
+                self.available_digits)) == 4
+
+            unique_chars = len(padded_guess_chars) == len(padded_guess)
+
+            if valid_chars and unique_chars:
+                self.last_returned_guess = n
+                return padded_guess
 
     def get_feedback(self, guess, bulls, cows):
-        # TODO: Do something with bulls, cows feedback?
-        pass
+        """
+        Act on feedback of "Bulls" (count of correct digits)
+        and "Cows" (count of misplaced digits)
+        B0, C0 => Totally wrong digits
+        B4, C0 => (We won)
+        B0, C4 => We have all the right digits, but totally wrong order...
+        B+C=4  => We have all the right digits, some in right order...
+
+        B1
+        """
+        if bulls == 0 and cows == 0:
+            self.available_digits = (
+                self.available_digits.difference(set(guess))
+            )
+                                    
--- test_player.py
+++ test_player.py
@@ -1,6 +1,7 @@
 from player import Player
 
-def test_player():
+
+def test_player_interface():
     # Make sure that the Player class has the expected interface
     p = Player()
     guess = p.guess()
@@ -10,4 +11,66 @@ def test_player():
     bulls = 0
     cows = 0
     p.get_feedback(guess, bulls, cows)
+
+
+# Possible improvement?
+# def test_random_guesses_are_valid():
+#     p = Player()
+#     for i in range(100):
+#         random_guess = p.random_guess()
+#         assert is_valid_guess(random_guess)
+
+# Valid Guesses:
+#   Are Strings
+#   Contain only numerical digits
+#       (can have a leading 0!)
+#   Have a length of 4, with no repeated digits
+
+def test_guesses_are_strings():
+    p = Player()
+    guess = p.guess()
+    assert isinstance(guess, str)
+
+
+def test_guess_is_length_4():
+    p = Player()
+    guess = p.guess()
+    assert len(guess) == 4
+
+
+def test_guess_doesnt_repeat_digits():
+    p = Player()
+    guess = p.guess()
+    assert len(set(guess)) == len(guess)
+
+
+def test_first_guess_is_first_valid_number():
+    p = Player()
+    guess = p.guess()
+    assert guess == "0123"
+
+
+def test_second_guess_is_the_second_valid_number():
+    p = Player()
+    p.guess()
+    second_guess = p.guess()
+    assert second_guess == "0124"
+
+
+def test_no_cattle_eliminates_digits():
+    p = Player()
+    assert len(p.available_digits) == 10
+    p.get_feedback("0123", 0, 0)
+    assert len(p.available_digits) == 6
+
+
+def test_no_cattle_eliminates_first_guess():
+    p = Player()
+    p.get_feedback("0123", 0, 0)
+    guess = p.guess()
+    assert guess != "0123"
+
+# possible strategy
+# def test_b_plus_c_equals_4_locks_current_digits():
+
 
